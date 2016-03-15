8c3bfc27e81e3aec7681198eba944621b3b1e92c
Philosoraptr <benj.smith19@gmail.com>
2016-03-16 07:41:15 +1300

    Create repository

---
+++ CastleScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CastleScript : MonoBehaviour {
+	public bool enemyCastle;
+	public bool playerCastle;
+	public float period = 0.1f;
+	public int castleHP;
+	public Text castleHPText;
+
+	private float nextActionTime = 0.0f;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if(enemyCastle || playerCastle) {
+			if (Time.time > nextActionTime){
+				nextActionTime = Time.time + period;
+				CastleHPIncrease();
+			}
+		}
+	}
+
+	void CastleHPIncrease() {
+		castleHP += 1;
+		castleHPText.text = castleHP.ToString();
+	}
+
+	void MouseInputHandler(){
+		//if (Input.touchCount == 1)
+		if(Input.GetMouseButtonDown (0))
+		{
+			if(playerCastle){
+				Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);//new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f
+				Vector2 touchPos = new Vector2(worldPoint.x, worldPoint.y);
+				if (GetComponent<Collider2D>().OverlapPoint(touchPos))
+				{
+					Debug.Log("Castle Touched");
+				}
+			}
+		}
+	}
+}
---
+++ InputHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour 
+{
+	public bool playerCastleTouched;
+	public GameObject touchedPlayerCastle;
+	public GameObject touchedBadCastle;
+
+	void Update()
+	{
+		//if (Input.touchCount == 1)
+		if(Input.GetMouseButtonDown (0))
+		{
+			Debug.Log("Mouse down");
+			Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);//new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f
+			Vector2 touchPos = new Vector2(worldPoint.x, worldPoint.y);
+			if (GetComponent<Collider2D>().OverlapPoint(touchPos))
+			{
+				playerCastleTouched = true;
+				Debug.Log("Castle Touched");
+			}
+		}
+	}
+}
